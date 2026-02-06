808cefbd12f54a59a74b046820cede5d9abc8709
lhn_wsl <pixelhn@qq.com>
2026-02-06 20:23:13 +0800

    add esp32 hosted fg driver

---
+++ sysdrv/drv_ko/wifi/esp32c3/insmod_esp_hosted_fg.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+RSETET_PIN=145
+SPI_HANDSHAKE_PIN=34
+SPI_DATAREADY_PIN=4
+SPI_CS_PIN=48
+SPI_CLOCKS_SPEED=10
+SPI_BUS=0
+SPI_MODE=0
+
+
+cd /oem/usr/ko/
+insmod esp32_spi_fg.ko resetpin=${RSETET_PIN} \
+    spi_bus=${SPI_BUS} \
+    spi_cs=${SPI_CS_PIN} \
+    spi_mode=${SPI_MODE} \
+    spi_handshake=${SPI_HANDSHAKE_PIN} \
+    spi_dataready=${SPI_DATAREADY_PIN} \
