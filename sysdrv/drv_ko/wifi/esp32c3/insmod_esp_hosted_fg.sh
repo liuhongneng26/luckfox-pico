50c637e9edfa56ef0ce6824a4065470b9afeda84
lhn_wsl <pixelhn@qq.com>
2026-03-08 00:15:44 +0800

    esp_hosted: clockspeed 20

--- sysdrv/drv_ko/wifi/esp32c3/insmod_esp_hosted_fg.sh
+++ sysdrv/drv_ko/wifi/esp32c3/insmod_esp_hosted_fg.sh
@@ -4,7 +4,7 @@ RSETET_PIN=145
 SPI_HANDSHAKE_PIN=34
 SPI_DATAREADY_PIN=4
 SPI_CS_PIN=0
-SPI_CLOCKS_SPEED=10
+SPI_CLOCKS_SPEED=20
 SPI_BUS=0
 SPI_MODE=0
 
@@ -16,3 +16,4 @@ insmod esp32_spi_fg.ko resetpin=${RSETET_PIN} \
     spi_mode=${SPI_MODE} \
     spi_handshake=${SPI_HANDSHAKE_PIN} \
     spi_dataready=${SPI_DATAREADY_PIN} \
+    clockspeed=${SPI_CLOCKS_SPEED}
